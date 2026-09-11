8f29f67427c7072a7620b0d5713d0dfa889e0819
Ellen Brage <ellen.brage@vgregion.se>
2026-09-11 07:42:28 +0200

    Add Vue support in packages

---
+++ packages/vue/src/components.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer, type StencilVueComponent } from '@stencil/vue-output-target/runtime';
+
+import type { JSX } from '@vgregion/components-core';
+
+
+
+
+
+export const VgrButton: StencilVueComponent<JSX.VgrButton> = /*@__PURE__*/ defineContainer<JSX.VgrButton>('vgr-button', undefined, [
+  'variant',
+  'disabled',
+  'type',
+  'vgrClick'
+], [
+  'vgrClick'
+]);
+
+
