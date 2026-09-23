9e0c85339e18438c75faaaaf4db2052670c38b7c
Ellen Brage <ellen.brage@vgregion.se>
2026-09-23 08:14:43 +0200

    Change from defineCustomElements to import script in Vue app

--- apps/vue-sandbox/src/main.ts
+++ apps/vue-sandbox/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import { defineCustomElements } from '@vgregion/components-core/loader'
+import '@vgregion/components-core/dist/components/define-all.js'
 import '@vgregion/components-core/dist/styles.css'
 import App from './App.vue'
 
-defineCustomElements()
 createApp(App).mount('#app')
